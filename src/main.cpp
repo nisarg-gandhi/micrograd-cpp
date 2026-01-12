8e5f06ed3b6eb18d7ab280ce7f6dbef76d563053
nisargg2211 <nisargg.1122@gmail.com>
2026-01-12 14:13:34 +0530

    feat: Add MLP architecture and refactor training loop
    Moved from manual weight initialization to a modular Neuron/Layer/MLP class structure. Simplified the SGD optimizer loop using parameter aggregation.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 #include <set>
 using namespace std;
 
+double random_uniform(double min, double max){
+    double r = (double) rand() / RAND_MAX;
+    return min + r * (max - min);
+}
 struct Value;
 
 class Value : public enable_shared_from_this<Value>
@@ -98,60 +102,125 @@ public:
     }
 };
 
-int main(){
-    
-    srand(time(0)); 
-    auto random_weight = []() {
-        return (rand() % 2000 / 1000.0) - 1.0; 
-    };
-
-    auto w1 = Value::create(random_weight()); 
-    auto w2 = Value::create(random_weight()); 
-    auto b1 = Value::create(random_weight());
-    
-    auto w3 = Value::create(random_weight()); 
-    auto w4 = Value::create(random_weight()); 
-    auto b2 = Value::create(random_weight());
-    
-    auto w5 = Value::create(random_weight()); 
-    auto w6 = Value::create(random_weight()); 
-    auto b3 = Value::create(random_weight());
-    
-    cout << "Initialized with random weights..." << endl;
-    
-    for (int k = 0; k < 1000; k++) {
-        
-        cout << "Step " << k << " ----------------" << endl;
-        double total_loss = 0;
-        double inputs[4][2] = {{0,0}, {0,1}, {1,0}, {1,1}};
-        double targets[4]   = {0,    1,    1,    0};
-        
-        for(int i=0; i<4; i++) {
-            
-            auto x1 = Value::create(inputs[i][0]);
-            auto x2 = Value::create(inputs[i][1]);
-            auto y_target = targets[i];
-            
-            auto n1 = ((w1*x1) + (w2*x2) + b1)->tanh();
-
-            auto n2 = ((w3*x1) + (w4*x2) + b2)->tanh();
-
-            auto score = (w5*n1) + (w6*n2) + b3;
-            
-            auto diff = score + Value::create(-y_target); // score - target
-            auto loss = diff * diff;
-            
-            total_loss += loss->data;
-            w1->grad=0; w2->grad=0; b1->grad=0;
-            w3->grad=0; w4->grad=0; b2->grad=0;
-            w5->grad=0; w6->grad=0; b3->grad=0;
-            
-            loss->backward();
-            double lr = 0.05;
-            w1->data -= lr * w1->grad; w2->data -= lr * w2->grad; b1->data -= lr * b1->grad;
-            w3->data -= lr * w3->grad; w4->data -= lr * w4->grad; b2->data -= lr * b2->grad;
-            w5->data -= lr * w5->grad; w6->data -= lr * w6->grad; b3->data -= lr * b3->grad;
+class Neuron {
+    public:
+    vector<shared_ptr<Value>> w;
+    shared_ptr<Value> b;
+    bool non_lin;
+    Neuron(int nin, bool non_lin=true): non_lin(non_lin){
+        for(int i = 0; i < nin; i++){
+            w.push_back(Value::create(random_uniform(-1.0, 1.0)));
         }
-        cout << "Total Loss: " << total_loss << endl;
+        b = Value::create(random_uniform(-1.0, 1.0));
     }
+
+    shared_ptr<Value>operator()(const vector<shared_ptr<Value>>&x){
+        auto act = b;
+        for(size_t i = 0; i<w.size();i++){
+            act = act + (w[i]*x[i]);
+        }
+        return non_lin?act->tanh():act;
+    }
+
+    vector<shared_ptr<Value>> parameters(){
+        vector <shared_ptr<Value>> params = w; 
+        params.push_back(b);
+        return params;
+    }
+};
+
+class Layer {
+    public:
+    vector <Neuron> neurons;
+    Layer(int nin, int nout, bool non_lin = true){
+        for(int i = 0; i<nout; i++){
+            neurons.emplace_back(nin, non_lin);
+        }
+    }
+
+    vector<shared_ptr<Value>> operator()(const vector<shared_ptr<Value>>&x){
+        vector <shared_ptr<Value>> outs; 
+        for(auto &n:neurons){
+            outs.push_back(n(x));
+        }
+        return outs;
+    }
+
+    vector <shared_ptr<Value>> parameters(){
+        vector<shared_ptr<Value>> params;
+        for(auto &n:neurons){
+            auto n_params = n.parameters();
+            for(auto &p:n_params){
+                params.push_back(p);
+            }
+        }
+        return params;
+    }
+};
+
+class MLP {
+    public:
+    vector <Layer> layers;
+    MLP(int nin, vector<int> nouts){
+        int sz = nouts.size();
+        for(int i = 0;i<sz;i++){
+            layers.emplace_back(i==0?nin:nouts[i-1], nouts[i], i!=sz-1);
+        }
+    }
+    vector<shared_ptr<Value>> operator()(vector<shared_ptr<Value>> x){
+        for(auto& layer:layers){
+            x = layer(x);
+        }
+        return x;
+    }
+    vector <shared_ptr<Value>> parameters(){
+        vector<shared_ptr<Value>> params;
+        for(auto& layer:layers){
+            auto l_params = layer.parameters();
+            for(auto&p :l_params){
+                params.push_back(p);
+            }
+        }
+        return params;
+    }
+};
+int main(){
+    srand(time(0));
+    vector<vector<double>> X = {
+        {2.0, 3.0, -1.0},
+        {3.0, -1.0, 0.5},
+        {0.5, 1.0, 1.0},
+        {1.0, 1.0, -1.0}
+    };
+    vector<double> Y = {1.0, -1.0, -1.0, 1.0};
+
+    MLP model(3, {4, 4, 1});
+
+    for(int k=0; k<200; k++) {
+
+        vector<shared_ptr<Value>> ypred;
+        for(auto& row : X) {
+            vector<shared_ptr<Value>> inputs;
+            for(double d : row) inputs.push_back(Value::create(d));
+            ypred.push_back(model(inputs)[0]); 
+        }
+        auto loss = Value::create(0.0);
+        for(size_t i=0; i<Y.size(); i++) {
+            auto diff = ypred[i] + Value::create(-Y[i]);
+            loss = loss + (diff * diff);
+        }
+
+        auto params = model.parameters();
+        for(auto& p : params) p->grad = 0.0;
+
+        loss->backward();
+
+        for(auto& p : params) {
+            p->data -= 0.05 * p->grad;
+        }
+
+        if (k % 10 == 0)
+            cout << "Step " << k << " loss: " << loss->data << endl;
+    }
+    
 }
